dd82d8d7059c0d68e4ec353884af0fb220b6173c
Sunil Suman <99410256+sunilsuman81@users.noreply.github.com>
2025-03-15 18:39:10 +0530

    Add files via upload

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/sks/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,18 @@
+package com.sks.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class TrackCoach implements Coach{
+
+    public TrackCoach() {
+        System.out.println("In Constructor "+ getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout() {
+        return "Practice warm up before run";
+    }
+}
