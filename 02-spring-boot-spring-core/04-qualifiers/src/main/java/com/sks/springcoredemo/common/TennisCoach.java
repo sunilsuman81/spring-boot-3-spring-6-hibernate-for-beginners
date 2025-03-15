83c4f0917c555bc927ff3b2814ab0b38e16eb8bb
Sunil Suman <99410256+sunilsuman81@users.noreply.github.com>
2025-03-15 18:31:08 +0530

    Add files via upload

---
+++ 02-spring-boot-spring-core/04-qualifiers/src/main/java/com/sks/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,11 @@
+package com.sks.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach{
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backend";
+    }
+}
